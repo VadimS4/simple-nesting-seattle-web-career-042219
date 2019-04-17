751a38c075c30d0e39f3645b08124c29442abf83
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-17 08:28:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,6 @@ def adding_to_dennis
         }
      }
      
-    return programmer_hash[:dennis_ritchie][:languages].("Assembly")
+    return programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
 end
