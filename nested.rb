a0da438ca76506cd42045f819ca0aab5a22c95b6
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-17 08:10:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+  return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    return programmer_hash[:dennis_ritchie][:languages].join("")
 end
 
 def adding_matz
@@ -66,9 +69,13 @@ def adding_matz
 
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
+ 		  :yukihiro_matsumoto => {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+ 		    },
+          :grace_hopper => {
+            :known_for => "COBOL",
+            :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
           :known_for => "Object Orientation",
@@ -101,7 +108,6 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
      
 end
 
