8c886f169d9981a5926b49d06adb06d1d033e4c4
Vadim Stakhnyuk <vstakhnyuk@outlook.com>
2019-04-17 08:29:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,7 +106,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-    return programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  return programmer_hash
 end
 
 def adding_to_dennis
